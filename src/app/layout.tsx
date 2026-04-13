7677cb910142286cb5f7fb07acaab52b4ae8a546
Jitto Joseph <jitto.joseph67@gmail.com>
2026-04-13 15:36:49 +0530

    use Geist Pixel Square font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistPixelSquare } from "geist/font/pixel";
 import Script from "next/script";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Jitto Joseph | Software Engineer Portfolio",
@@ -113,9 +103,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${GeistPixelSquare.variable} antialiased`}>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-F0REMK7YMM"
           strategy="afterInteractive"
